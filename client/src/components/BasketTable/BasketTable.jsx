b5e0a63fdb19cb480595b285cad592f4a3a3fd21
Владислав Петров <vp.vlad00@mail.ru>
2023-04-08 18:25:51 +0300

    added +/- in basketPage

--- client/src/components/BasketTable/BasketTable.jsx
+++ client/src/components/BasketTable/BasketTable.jsx
@@ -1,22 +1,66 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import BasketTableRow from '../BasketTableRow/BasketTableRow';
 import BasketTableHeader from '../BasketTableHeader/BasketTableHeader';
-import { observer } from 'mobx-react-lite';
-import { Context } from '../../index';
 import { uid } from 'uid';
 import './BasketTable.css';
+// import { Context } from '../../index';
 
-const BasketTable = observer(() => {
-    const { basket } = useContext(Context);
+const BasketTable = () => {
+    // const { basketItem } = useContext(Context);
+    const [bsk] = useState(() => {
+        const storedBasket = localStorage.getItem('basket');
+        return storedBasket !== null ? JSON.parse(storedBasket) : [];
+    });
+
+    const sum = (arr) => {
+        let res = 0;
+        for (let i = 0; i < arr.length; i++) {
+            res += Number(arr[i].price * arr[i].count);
+        }
+        return res;
+    }
+
+    const handleSend = (event) => {
+        event.preventDefault();
+        alert("Информация о заказе поступит на почту!");
+    }
+
+    useEffect(() => {
+        localStorage.setItem('basket', JSON.stringify(bsk))
+    }, [bsk]);
 
     return (
-        <div className="table">
+        <form className="table" onSubmit={handleSend}>
             <BasketTableHeader />
-            {basket.items.map((item) => (
-                <BasketTableRow key={uid()} item={item} />
-            ))}
-        </div>
+            {bsk.length > 0 ?
+                bsk.map((item) => (
+                    <BasketTableRow key={uid()} item={item} />
+                ))
+                :
+                <div className="empty__basket">Корзина пуста</div>
+            }
+            <div className="sum">
+                Итого: {sum(bsk)}
+            </div>
+            <div className="btn__container">
+                {bsk.length > 0 ?
+                    <input
+                        className="done__button"
+                        type="submit"
+                        value="Оформить заказ"
+                    />
+                    :
+                    <input
+                        className="done__button-disabled"
+                        type="submit"
+                        value="Оформить заказ"
+                        disabled
+                    />
+                }
+
+            </div>
+        </form>
     );
-});
+};
 
 export default BasketTable;
