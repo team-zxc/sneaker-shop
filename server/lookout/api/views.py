61e96f0bafb04ad442062e7a862c4eb56b0aa156
Владислав Петров <vp.vlad00@mail.ru>
2023-07-01 23:12:17 +0300

    fix send message

--- server/lookout/api/views.py
+++ server/lookout/api/views.py
@@ -14,8 +14,10 @@ from rest_framework.views import APIView
 
 from rest_framework.generics import RetrieveAPIView
 
-from .models import Footwear, Brand
+from .models import Footwear, Brand, Images
 from .serializers import BrandSerializer, FootwearSerializer
+
+from django.forms.models import model_to_dict
 
 
 class FootwearAPIView(ListAPIView):
@@ -59,26 +61,106 @@ class SingleFootwearAPIView(RetrieveAPIView):
 @api_view(['POST'])
 def create_order(request):
     data = json.loads(request.body)
+    # Получение имени пользователя из данных запроса
+    name = data['contacts'].get('name', 'Пользователь')
 
     msg = MIMEMultipart()
 
     msg['To'] = data['contacts']['email']
-    msg['Subject'] = "Новый заказ"
+    msg['Subject'] = "Новый заказ Lookout"
 
-    msg.attach(MIMEText(str(data['items']), 'plain'))
+    def get_item_data_by_id(item_id):
+        try:
+            footwear = Footwear.objects.get(id=item_id)
+            # Извлечение нужных данных из объекта footwear
+            item_data = model_to_dict(footwear)
+            item_data['category'] = footwear.category.title
+            item_data['brand'] = footwear.brand.title
+            item_data['model'] = footwear.model.title
+            # Получение ссылок на картинки связанные с footwear
+            images = Images.objects.filter(footwear=footwear)
+            image_urls = [image.image.url for image in images]
 
+            item_data['images'] = image_urls
+            # Преобразование значений строк в юникод для поддержки кириллицы
+            item_data_unicode = {key: str(value) for key, value in item_data.items()}
+            return item_data_unicode
+        except Footwear.DoesNotExist:
+            return None
+
+    items = data['items']
+    items_with_data = []
+    for item in items:
+        item_id = item['id']
+        item_data_unicode = get_item_data_by_id(item_id)  # Здесь получаем все данные элемента по его id из другой таблицы
+        item_with_data = {
+            'id': item_id,
+            'data': item_data_unicode,
+            'size': item['size'],
+            'count': item['count'],
+            'images': item_data_unicode['images'] if item_data_unicode else []
+        }
+        items_with_data.append(item_with_data)
+
+    # Формирование текста сообщения
+    message = f"Добрый день, {name}!\n\nВы оформили заказ:\n"
+
+    for item_with_data in items_with_data:
+        category = item_with_data['data']['category']
+        brand = item_with_data['data']['brand']
+        model = item_with_data['data']['model']
+        item_name = item_with_data['data']['name']
+        size = item_with_data['size']
+        count = item_with_data['count']
+
+        item_message = f"{category} {brand} {model} {item_name} размером {size} в количестве {count}"
+        message += item_message + "\n"
+
+    message += "\nОжидайте, с вами свяжется продавец по номеру или почте.\n\nС уважением, Владислав Петров\nКонтакты:\nvp.vlad00@mail.ru\n89035101495\n@vinatian00"
+
+    # Установка содержимого сообщения в формате HTML
+    msg.attach(MIMEText(message, 'plain'))
 
     # Поменять в зависимости от почты
     server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-    password = "sdasdasdada"
+    password = "tS2RENcsg9Reu4xyJ9kX"
     msg['From'] = "vp.vlad00@mail.ru"
 
 #     server.starttls()
 
     server.login(msg['From'], password)
 
+#   Отправка сообщения покупателю
     server.sendmail(msg['From'], msg['To'], msg.as_string())
-    server.sendmail(msg['From'], msg['From'], msg.as_string())
+
+    # Формирование сообщения для продавца
+    seller_message = f"Поступил заказ от заказчика - {name}:\n\n"
+    for item_with_data in items_with_data:
+        category = item_with_data['data']['category']
+        brand = item_with_data['data']['brand']
+        model = item_with_data['data']['model']
+        item_name = item_with_data['data']['name']
+        size = item_with_data['size']
+        count = item_with_data['count']
+        vendor_code = item_with_data['data'].get('vendor_code', '')
+
+        item_message = f"{category} {brand} {model} {item_name} размером {size} в количестве {count}. Артикул: {vendor_code}"
+        seller_message += item_message + "\n"
+
+    seller_message += "\nКонтакты для связи с заказчиком:\n"
+    seller_message += data['contacts']['email'] + "\n"
+    seller_message += data['contacts']['phone'] + "\n"
+
+    # Отправка письма продавцу
+    seller_msg = MIMEMultipart()
+    seller_msg['From'] = msg['From']
+    seller_msg['To'] = msg['From']
+    seller_msg['Subject'] = "Lookout. Новый заказ"
+
+    # Установка содержимого сообщения для продавца
+    seller_msg.attach(MIMEText(seller_message, 'plain'))
+
+    server.sendmail(seller_msg['From'], seller_msg['To'], seller_msg.as_string())
 
     server.quit()
 
